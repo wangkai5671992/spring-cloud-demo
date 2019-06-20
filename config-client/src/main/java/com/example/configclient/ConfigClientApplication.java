72715bf177ca40599ad53201c88dbbc6ced82553
王凯 <3172609@qq.com>
2019-06-20 20:15:26 +0800

    configer

---
+++ config-client/src/main/java/com/example/configclient/ConfigClientApplication.java
@@ -0,0 +1,23 @@
+package com.example.configclient;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class ConfigClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApplication.class, args);
+    }
+
+    @Value("${foo}")
+    private String foo;
+
+    @Bean
+    CommandLineRunner doSomething() {
+        return args -> System.out.println(foo);
+    }
+}
